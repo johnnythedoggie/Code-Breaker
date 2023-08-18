f98ab7d620f3012239ee4dec4ceb51af6e1f2b41
Jason Turner <jasontisawesome@gmail.com>
2023-08-17 17:52:57 -0700

    Initial Commit

---
+++ Master Code Breaker/Master_Code_BreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Master_Code_BreakerApp.swift
+//  Master Code Breaker
+//
+//  Created by Jason Turner on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Master_Code_BreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
